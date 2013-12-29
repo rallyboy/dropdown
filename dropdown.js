a0212fbb4c62a1c1cdc94cf1d09630367fab9bec
Alex Tsouloftas <i@alex.ms>
2013-12-29 17:20:52 +0200

    Version 0.2

--- dropdown.js
+++ dropdown.js
@@ -1,110 +1,140 @@
 function dropdown() {
+    var ddtimer;
 
     $("select.dd").each(function () {
         var th = $(this);
-        if (th.parent().hasClass("dd")){return false;}
-      
-      
-        var id = "dd" + Math.round(9999999 * Math.random());
-        var d = "." + id;
-        
-        var width = th.width();
-        var atid = $(th).attr("id");
-      
-        
-        
-        th.after("<div style='cursor:pointer' class='dd " + id + "'/>");
-      
-        if (typeof atid != 'undefined'){
-          th.attr("data-id",atid).removeAttr("id");
-          $(d).attr("id",atid); 
+        if (th.parent().hasClass("dd")) {
+            return false;
         }
 
-        th.css({"position": "absolute"}).hide()
-        $("<dt>" + th.val() + "</dt>").appendTo(d);
+
+        var id = "dd" + Math.round(999 * Math.random());
+        var d = "." + id;
+
+        var width = th.width();
+        var atid = $(th).attr("id");
+
+        th.after("<div style='cursor:pointer' bg='" + th.css("background-color") + "' class='dd " + id + "'/>");
+
+        if (typeof atid != 'undefined') {
+            th.attr("data-id", atid).removeAttr("id");
+            $(d).attr("id", atid);
+        }
+        th.hide()
+        $('<dt><span class="ddtxt">' + th.val() + '</span><span class="ddarrow" style="width:0;height:0;border:8px solid transparent;float:right;position:absolute;right:15px;top:50%"></span></dt>').appendTo(d);
         $(d).css("max-width", width * 1.5);
         th.appendTo(d);
-        
-        $("<div class='ddown' style='display:none;position:absolute;z-index:99999'/>").appendTo(d);
-        var span = '<span style="width: 0px;height: 0px;border-style: solid;border-width: 8px 7.5px 0 7.5px;loat:right;transition : border 150ms ease-out;"></span>';
-        $(span).appendTo(d + " dt");
-      
-        
-        th.find("option").each(function () {
+
+        $("<div class='ddown' style='display:none;position:absolute;z-index:9999;' />").appendTo(d);
+
+        $(d+" option").each(function () {
             var val = $(this).val();
             $(d + " .ddown").append("<li style='list-style:none'>" + val + "</li>");
         });
 
-        var slideTimer;
 
-      function close(){
-        slideTimer = setInterval(function() {
-               $(d + " .ddown").fadeOut(200);
-                $(d).find("dt").css("background","white");
-            }, 800);
-      }
 
-      
-        // OPEN DROPDOWN
-        $(d + " dt").on("click", function (e) {
-          clearInterval(slideTimer);
-          e.stopPropagation();
-          
-          if ($(d + " .ddown").is(":visible")){
-            $(d + " .ddown").fadeOut("fast");
-            $(d).find("dt").css("background","white");
-            return false;
-          }
-            $(".dd .ddown").hide();
-            $(this).css("background","#eee");
-            var s = $(d + " dt").text();          
-            $(d + " .ddactive").removeClass("ddactive");            
-            $(d + " .ddown").css("width",$(d).width()).fadeIn(200)
-            .find("li:contains('"+s+"')")
-            .addClass("ddactive");
-          
-            
-            close();
-          
-            $(d + " .ddown").hover(function(){
-              clearInterval(slideTimer);
-            },function(){
-              clearInterval(slideTimer);
-              close();
-            });
-          });
-          
-      
-        // CLOSE DROPDOWN
-        $(d + " li").on("click", function (e) {
-            var newid = $(this).text();
-            $(d + " dt").text(newid);
-            $(d + " .ddown").fadeOut(200);
-            $(d + " select").val(newid);
-            $(d + " dt").append(span);
-        });
-        
-      
-      
-        // OUTSIDE BOX CLOSE
-        $(document).click(function () {
-            $(d + " .ddown").fadeOut("fast");
-            $(d).find("dt").css("background","white");
-        });
 
-       
+    }); // END OF EACH
 
-      
-          
-    
-      
-      
-      
-      
-      
-      
+    function l() {
+        if (!$(".ddown").is(":visible")) {
+            return false
+        }
+        clearInterval(ddtimer);
+        //console.log("#008")
+        var x = "." + ($(".ddown:visible").parent().attr("class")).match(/(^|\s)(dd\d+)($|\s)/)[2];
+        $(".ddown").hide();
+
+        $(x).find("dt").css("background", $(x).attr("bg"));
+        return false;
+    }
+
+  
+
+    $(".ddown").hover(function () {
+        clearInterval(ddtimer);
+        //console.log("#002")
+    }, function () {
+        clearInterval(ddtimer);
+        //console.log("#007")
+        close();
     });
-}
+    
+
+
+    function close() {
+        if (!$(".ddown").is(":visible")) {
+            return false
+        }
+        //console.log("#009")
+        ddtimer = setTimeout(function () {
+            l();
+        }, 800);
+    }
+
+
+
+
+    // OPEN DROPDOWN
+    $(".dd").on("click", function (e) {
+        var x = "." + ($(this).attr("class")).match(/(^|\s)(dd\d+)($|\s)/)[2];
+
+        clearInterval(ddtimer);
+        e.stopPropagation();
+
+        if ($(x + " .ddown").is(":visible")) {
+            $(x + " .ddown").hide();
+            $(x).find("dt").css("background", $(x).attr("bg"));
+            return false;
+        }
+
+        if ($(".ddown").is(":visible")) {
+            $(".ddown").hide();
+        }
+
+        //console.log("#004")
+
+
+        var s = $(x + " dt").text();
+        $(x + " .ddactive").removeClass("ddactive");
+        $(x + " .ddown").css("width", $(x).width()).show()
+            .find("li:contains('" + s + "')")
+            .addClass("ddactive");
+
+
+        //$(".dd .ddown").hide();
+        //$(this).css("background", "#eee");
+
+    });
+
+    
+
+    // CLOSE DROPDOWN
+    $(".ddown li").on("click", function (e) {
+        var d = "." + ($(this).parent().parent().attr("class")).match(/(^|\s)(dd\d+)($|\s)/)[2];
+
+        var newid = $(this).text();
+        $(d + " .ddtxt").text(newid);
+        $(d + " select").val(newid);
+        l();
+        return false;
+    });
+
+
+
+
+    // OUTSIDE BOX CLOSE
+    $(document).click(function () {
+        if ($(".ddown").is(":visible")) {
+            l();
+            return false
+        }
+    });
+
+
+} // END OF DROPDOWN
+
 
 
 dropdown()
